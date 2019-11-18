6fed9ed2963d4809c3f72a66bf1e01f152667177
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-17 22:30:00 -0500

    Update

--- imle_2D.py
+++ imle_2D.py
@@ -146,7 +146,7 @@ def main(*args):
 
     # restore data
     temp = np.load("../Zeldovich_Approximation.npz")
-    sim_z0 = temp["sim_z0"] + 1.
+    sim_z0 = temp["sim_z0"] + 5.
     sim_z0 = sim_z0.reshape(sim_z0.shape[0]*sim_z0.shape[1],1,sim_z0.shape[2],sim_z0.shape[3])
     train_data = sim_z0[::100,:,:,:]
     print(train_data.shape)
