df73a9d5b010a910715e3e6567042da3b583b289
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-19 19:57:15 -0500

    Update

--- imle_2D.py
+++ imle_2D.py
@@ -43,8 +43,8 @@ class IMLE():
         self.dci_db = None
 
 #-----------------------------------------------------------------------------------------------------------
-    def train(self, data_np, base_lr=1e-3, batch_size=512, num_epochs=5000,\
-              decay_step=25, decay_rate=1.0, staleness=500, num_samples_factor=30):
+    def train(self, data_np, base_lr=1e-3, batch_size=512, num_epochs=1800,\
+              decay_step=25, decay_rate=1.0, staleness=300, num_samples_factor=30):
 
         # define metric
         loss_fn = nn.MSELoss().cuda()
