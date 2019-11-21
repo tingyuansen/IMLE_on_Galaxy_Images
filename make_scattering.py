b9cfdd2c3691bd84fd947bf1e8a1f90b42da3880
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-20 19:16:26 -0500

    Update

--- make_scattering.py
+++ make_scattering.py
@@ -26,7 +26,7 @@ def main():
     scattering.cuda()
 
     # transform to torch tensors
-    tensor_training_x = torch.from_numpy(training_data).type(torch.cuda.FloatTensor)
+    tensor_training_x = torch.from_numpy(train_data).type(torch.cuda.FloatTensor)
 
     # perform scattering
     Sx = scattering(tensor_training_x).mean(dim=(2,3)).log().cpu().detach().numpy()
