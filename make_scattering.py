15b5b09e6d8e35cd22eff808a76dc519d34c35b9
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-20 19:26:17 -0500

    Update

--- make_scattering.py
+++ make_scattering.py
@@ -22,7 +22,7 @@ def main():
     training_x = temp["training_data"]
 
     # define scattering
-    scattering = Scattering2D(J=5, shape=(training_x[0,:,:].shape), L=4, max_order=2)
+    scattering = Scattering2D(J=4, shape=(training_x[0,:,:].shape), L=4, max_order=2)
     scattering.cuda()
 
     # initiate results array
@@ -51,7 +51,7 @@ def main():
         except:
             Sx_array = Sx[i]
         print(Sx_array.shape)
-    np.save("Sx_Illustris_Images.npy", Sx_array)
+    np.save("../Sx_Illustris_Images.npy", Sx_array)
 
 
 #=========================================================================================================
