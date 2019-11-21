5051d0b0c67e7ddb8d38a7c528bfb21256bd7273
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-20 20:07:10 -0500

    Update

--- imle_2D_scattering.py
+++ imle_2D_scattering.py
@@ -176,7 +176,7 @@ def main(*args):
     temp = np.load("../Illustris_Images.npz")
     train_data = temp["training_data"][::10,None,32:-32,32:-32]
     train_data = np.clip(np.arcsinh(train_data)+0.05,0,5)/5
-    train_data = np.log(train_data) + 5.
+    # train_data = np.log(train_data) + 5.
     print(train_data.shape)
 
     # restore scattering coefficients
