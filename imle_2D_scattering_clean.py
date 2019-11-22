1f5643e4f0df1ccf9d9c1db6eeb6f92b05c6ff7b
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-22 18:35:12 -0500

    Update

--- imle_2D_scattering_clean.py
+++ imle_2D_scattering_clean.py
@@ -14,34 +14,57 @@ from dci import DCI
 # define network
 class ConvolutionalImplicitModel(nn.Module):
     def __init__(self, z_dim):
-        super( ConvolutionalImplicitModel, self).__init__()
+        super(ConvolutionalImplicitModel, self).__init__()
         self.z_dim = z_dim
-
-        layers = []
-
-        for i in range(5):
-            for j in range(1):
-
-                if i == 0 and j == 0:
-                    layers.append(torch.nn.ConvTranspose2d(z_dim, 512, 4, stride=1, padding=0))
-                    layers.append(torch.nn.BatchNorm2d(512, momentum=0.001, affine=False))
-                    layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
-                else:
-                    layers.append(torch.nn.Conv2d(512, 512, 5, stride=1, padding=2))
-                    layers.append(torch.nn.BatchNorm2d(512, momentum=0.001, affine=False))
-                    layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
-
-            if i < 4:
-                layers.append(torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners = False))
-            else:
-                layers.append(torch.nn.Conv2d(512, 1, 5, stride=1, padding=2))
-                layers.append(torch.nn.LeakyReLU())
-
-        self.model = torch.nn.Sequential(*layers)
-        self.add_module("model", self.model)
+        self.tconv1 = nn.ConvTranspose2d(z_dim, 1024, 1, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(1024)
+        self.tconv2 = nn.ConvTranspose2d(1024, 128, 7, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.tconv3 = nn.ConvTranspose2d(128, 64, 4, 3, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.tconv4 = nn.ConvTranspose2d(64, 1, 4, 3, padding=2, output_padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(1)
+        self.relu = nn.LeakyReLU()
 
     def forward(self, z):
-        return self.model(z)
+        z = self.relu(self.bn1(self.tconv1(z)))
+        z = self.relu(self.bn2(self.tconv2(z)))
+        z = self.relu(self.bn3(self.tconv3(z)))
+        z = self.relu(self.bn4(self.tconv4(z)))
+        return z
+
+#-----------------------------------------------------------------------------------------------------------
+# # define network
+# class ConvolutionalImplicitModel(nn.Module):
+#     def __init__(self, z_dim):
+#         super( ConvolutionalImplicitModel, self).__init__()
+#         self.z_dim = z_dim
+#
+#         layers = []
+#
+#         for i in range(5):
+#             for j in range(1):
+#
+#                 if i == 0 and j == 0:
+#                     layers.append(torch.nn.ConvTranspose2d(z_dim, 512, 4, stride=1, padding=0))
+#                     layers.append(torch.nn.BatchNorm2d(512, momentum=0.001, affine=False))
+#                     layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
+#                 else:
+#                     layers.append(torch.nn.Conv2d(512, 512, 5, stride=1, padding=2))
+#                     layers.append(torch.nn.BatchNorm2d(512, momentum=0.001, affine=False))
+#                     layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
+#
+#             if i < 4:
+#                 layers.append(torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners = False))
+#             else:
+#                 layers.append(torch.nn.Conv2d(512, 1, 5, stride=1, padding=2))
+#                 layers.append(torch.nn.LeakyReLU())
+#
+#         self.model = torch.nn.Sequential(*layers)
+#         self.add_module("model", self.model)
+#
+#     def forward(self, z):
+#         return self.model(z)
 
 
 #=============================================================================================================
@@ -147,12 +170,12 @@ class IMLE():
 #-----------------------------------------------------------------------------------------------------------
             # save the mock sample
             if (epoch+1) % staleness == 0:
-                np.savez("../results_2D_j=1_clean.npz", data_np=data_np, Sx_np=Sx_np,\
+                np.savez("../results_2D_simple_clean.npz", data_np=data_np, Sx_np=Sx_np,\
                                 samples_np=samples_predict)
 
                 # make random mock
                 samples_random = self.model(z_Sx_all[:10**3]).cpu().data.numpy()
-                np.savez("../results_2D_random_j=1_clean.npz", samples_np=samples_random)
+                np.savez("../results_2D_random_simple_clean.npz", samples_np=samples_random)
 
 
 #=============================================================================================================
@@ -177,7 +200,7 @@ def main(*args):
 
     # train the network
     imle.train(train_data, train_Sx)
-    torch.save(imle.model.state_dict(), '../net_weights_2D_j=1_clean.pth')
+    torch.save(imle.model.state_dict(), '../net_weights_2D_simple_clean.pth')
 
 #---------------------------------------------------------------------------------------------
 if __name__ == '__main__':
