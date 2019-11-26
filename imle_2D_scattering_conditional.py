45032a37c1294c242f2d64ac9bef2ec25cb3dd83
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-25 23:34:55 -0500

    Update

--- imle_2D_scattering_conditional.py
+++ imle_2D_scattering_conditional.py
@@ -95,7 +95,7 @@ class IMLE():
 
 #-----------------------------------------------------------------------------------------------------------
     def train(self, data_np, data_Sx, base_lr=1e-3, batch_size=128, num_epochs=3000,\
-              decay_step=25, decay_rate=0.95, staleness=100, num_samples_factor=30):
+              decay_step=25, decay_rate=0.95, staleness=100, num_samples_factor=100):
 
         # define metric
         loss_fn = nn.MSELoss().cuda()
@@ -194,17 +194,17 @@ class IMLE():
 #-----------------------------------------------------------------------------------------------------------
             # save the mock sample
             if (epoch+1) % staleness == 0:
-                np.savez("../results_2D_conditional_times10.npz", data_np=data_np,\
+                np.savez("../results_2D_conditional_times3_trial2_epoch=" + str(epoch) +  ".npz", data_np=data_np,\
                                 z_Sx_np=z_Sx.cpu().data.numpy(),\
                                 samples_np=samples_predict)
 
                 # make random mock
                 samples_random = self.model(z_Sx_all[:10**4][::100]).cpu().data.numpy()
-                np.savez("../results_2D_random_conditional_times10.npz", samples_np=samples_random,
+                np.savez("../results_2D_random_conditional_times3_trial2_epoch=" + str(epoch) +  ".npz", samples_np=samples_random,
                           mse_err=err / num_batches)
 
                 # save network
-                torch.save(self.model.state_dict(), '../net_weights_2D_conditional_times10_epoch=' \
+                torch.save(self.model.state_dict(), '../net_weights_2D_conditional_times3_trial2_epoch=' \
                              + str(epoch) + '.pth')
 
 #=============================================================================================================
