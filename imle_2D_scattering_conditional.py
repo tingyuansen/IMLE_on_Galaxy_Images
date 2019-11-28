43763a812e7ad47c329a5d7175286a59815f6b2c
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-27 22:59:23 -0500

    Update

--- imle_2D_scattering_conditional.py
+++ imle_2D_scattering_conditional.py
@@ -214,7 +214,7 @@ def main(*args):
 
     # restore data
     temp = np.load("../Illustris_Images.npz")
-    train_data = temp["training_data"][::10,None,32:-32,32:-32]
+    train_data = temp["training_data"][:,None,32:-32,32:-32]
     train_data = np.clip(np.arcsinh(train_data)+0.05,0,5)/5
     print(train_data.shape)
 
