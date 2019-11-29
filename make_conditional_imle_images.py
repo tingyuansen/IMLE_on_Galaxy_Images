78e9327cf5a90f3b7c2ebf77892900e711d21121
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-28 22:16:48 -0500

    Update

--- make_conditional_imle_images.py
+++ make_conditional_imle_images.py
@@ -142,16 +142,16 @@ def main(*args):
     print(train_data.shape)
 
     # # restore scattering coefficients
-    # train_Sx = np.load("../Sx_Illustris_Images.npy")[::30,:,None,None]
-    # print(train_Sx.shape)
+    train_Sx = np.load("../Sx_Illustris_Images.npy")[::30,:,None,None]
+    print(train_Sx.shape)
 
     # make low resolution as conditional
-    train_Sx = np.empty((train_data.shape[0],)+(1,16,16))
-    for i in range(train_data.shape[0]):
-        for j in range(16):
-            for k in range(16):
-                train_Sx[i,:,j,k] = np.mean(train_data[i,0,j*4:(j+1)*4,k*4:(k+1)*4])
-    train_Sx = train_Sx.reshape(train_Sx.shape[0],np.prod(train_Sx.shape[1:]),1,1)
+    # train_Sx = np.empty((train_data.shape[0],)+(1,16,16))
+    # for i in range(train_data.shape[0]):
+    #     for j in range(16):
+    #         for k in range(16):
+    #             train_Sx[i,:,j,k] = np.mean(train_data[i,0,j*4:(j+1)*4,k*4:(k+1)*4])
+    # train_Sx = train_Sx.reshape(train_Sx.shape[0],np.prod(train_Sx.shape[1:]),1,1)
 
 #---------------------------------------------------------------------------------------------
     # initiate network
