5f438c438944d9e236d7ae05d3d7bc070240f27e
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-20 19:57:50 -0500

    Update

--- real_nvp_1D_spectra.py
+++ real_nvp_1D_spectra.py
@@ -23,19 +23,22 @@ from astropy.io import fits
 # y_tr[y_tr > 1.5] = 1.
 
 #-------------------------------------------------------------------------------------------------------
+# read H3 labels
+hdulist = fits.open('../H3_labels.fits')
+SNR = hdulist[1].data['SNR']
+
+# read H3 spectra
 # H3 id, wave, flux, err, model, rest_wave
 hdulist = fits.open('../H3_spectra.fits')
 temp = hdulist[1].data
 
 flux_spectra = np.empty((len(temp),temp[0][1].size))
-model_spectra = np.empty((len(temp),temp[0][1].size))
 for i in range(flux_spectra.shape[0]):
     flux_spectra[i,:] = temp[i][2]
-    model_spectra[i,:] = temp[i][4]
 
 # cull empty spectra
 median_flux = np.median(flux_spectra, axis=1)
-flux_spectra = flux_spectra[median_flux != 0,:]
+flux_spectra = flux_spectra[(median_flux != 0)*(SNR > 10.),:]
 
 # exclude pixels
 flux_spectra = flux_spectra[:,100:-100]
