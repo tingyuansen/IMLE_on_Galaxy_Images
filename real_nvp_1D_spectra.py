7601019f3c0d07622df90c66ec94c5b107c80005
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-21 21:33:22 -0500

    Update

--- real_nvp_1D_spectra.py
+++ real_nvp_1D_spectra.py
@@ -49,7 +49,7 @@ for i in range(flux_spectra.shape[0]):
     err_array[i] = np.median(temp[i][2]/temp[i][3])
 
 # cull empty spectra
-ind = np.where((np.median(flux_spectra, axis=1) != 0)*(err_array > 20.)*(h3_flag == 0) == 1)[0]
+ind = np.where((np.median(flux_spectra, axis=1) != 0)*(err_array > 10.)*(h3_flag == 0) == 1)[0]
 flux_spectra = flux_spectra[ind,:]
 
 # normalize spectra
@@ -183,7 +183,7 @@ for e in range(num_epochs):
 
 #========================================================================================================
 # save models
-torch.save(flow, 'flow_final_lr=-4_SNR=20.pt')
+torch.save(flow, 'flow_final_lr=-4_SNR=10.pt')
 
 # sample results
 z1 = np.empty(y_tr.shape)
@@ -197,8 +197,8 @@ for i in range(nbatches):
     = flow.sample(z1_tr[i*batch_size:(i+1)*batch_size]).detach().cpu().numpy()
 
 # save results
-np.savez("../real_nvp_results_lr=-4_SNR=20.npz",\
+np.savez("../real_nvp_results_lr=-4_SNR=10.npz",\
          z1 = z1,\
          x1 = x1)
-np.savez("../loss_results_lr=-4_SNR=20.npz",\
+np.savez("../loss_results_lr=-4_SNR=10.npz",\
          loss_array = loss_array)
