57c8d3af94141a127b8973c53b19ff5ae7af6a7c
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-24 11:04:19 -0500

    Update

--- imle_1D_spectra_conditional.py
+++ imle_1D_spectra_conditional.py
@@ -107,7 +107,7 @@ class IMLE():
         self.dci_db = None
 
 #-----------------------------------------------------------------------------------------------------------
-    def train(self, data_np, data_Sx, base_lr=1e-4, batch_size=128, num_epochs=3000,\
+    def train(self, data_np, data_Sx, base_lr=1e-4, batch_size=256, num_epochs=3000,\
              decay_step=25, decay_rate=0.95, staleness=100, num_samples_factor=100):
 
         # define metric
