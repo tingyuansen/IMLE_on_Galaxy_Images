5ebe6415ce6bc2b13547c85c6cd4322809f94a22
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-24 11:25:54 -0500

    Update

--- imle_1D_spectra_conditional_predict_spectra.py
+++ imle_1D_spectra_conditional_predict_spectra.py
@@ -74,36 +74,28 @@ model.load_state_dict(state_dict)
 Sx = torch.from_numpy(train_Sx).float().cuda()
 
 #========================================================================================================
-### predict with a single latent z ###
-# z = torch.zeros(Sx.shape[0], z_dim).cuda()
-# z_Sx_all = torch.cat((z, Sx), axis=1)[:,:,None]
-#
-# # train in batch
-# batch_size = 100
-# num_batches = z_Sx_all.shape[0] // batch_size
-# predict_flux_array = []
-# for i in range(num_batches):
-#     print(i)
-#     predict_flux_array.extend(model.forward(z_Sx_all[i*batch_size:(i+1)*batch_size]).cpu().data.numpy())
-# predict_flux_array = np.array(predict_flux_array)
-
-
-#========================================================================================================
 ### predict with random z and find the best estimates ###
+
+# # train in batch
+batch_size = 100
+num_batches = Sx.shape[0] // batch_size
 num_samples_factor = 100
-Sx = torch.from_numpy(np.repeat(train_Sx,num_samples_factor,axis=0)).float().cuda()
-z = torch.zeros(Sx.shape[0], z_dim).cuda()
-z_Sx_all = torch.cat((z, Sx), axis=1)[:,:,None]
 
-predict_flux_array_all = np.empty((train_data.shape[0]*num_samples_factor,train_data.shape[1]))
+predict_flux_array = []
+for j in range(num_samples_factor):
+    for i in range(num_batches):
+        print(j,i)
+        z = torch.randn(Sx.shape[0], z_dim).cuda()
+        z_Sx_all = torch.cat((z, Sx), axis=1)[:,:,None]
+        predict_flux_array.extend(model.forward(z_Sx_all[i*batch_size:(i+1)*batch_size]).cpu().data.numpy())
+predict_flux_array_all = np.copy(np.array(predict_flux_array))
+print(predict_flux_array_all.shape)
+print(predict_flux_array_all[0::num_sample_factor])
+
+predict_flux_array = []
 for i in range(train_Sx.shape[0]):
     print(i)
-    predict_flux_array_all[i::num_samples_factor]\
-        = model.forward(z_Sx_all[i::num_samples_factor]).cpu().data.numpy()
-
-for i in range(train_Sx.shape[0]):
-    print(i)
-    predict_flux_temp = predict_flux_array_all[i*num_sample_factor:(i+1)*num_sample_factor]
+    predict_flux_temp = predict_flux_array_all[i::num_sample_factor]
     diff = np.sum((predict_flux_temp-train_data[i])**2,axis=1)
     predict_flux_array.append(predict_flux_temp[np.argmin(diff),:])
 predict_flux_array = np.array(predict_flux_array)
