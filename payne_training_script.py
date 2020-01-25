4ef06266cbe5b89aa044711c889d1538debcff78
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-24 22:58:41 -0500

    Update

--- payne_training_script.py
+++ payne_training_script.py
@@ -6,7 +6,8 @@ from The_Payne import training
 #==========================================================================================
 # read spectra
 temp = np.load("../H3_training_grid.npz")
-spectra = temp["spectra"][:,150:4433+150]
+#spectra = temp["spectra"][:,150:4433+150]
+spectra = temp["spectra"][:,150:4375+150]
 labels = temp["labels"]
 labels[:,0] = labels[:,0]/1000.
 
@@ -16,7 +17,7 @@ print(labels.shape)
 # reshuffle the array
 ind_shuffle = np.arange(spectra.shape[0])
 np.random.shuffle(ind_shuffle)
-np.savez("ind_shuffle_payne_h3.npz", ind_shuffle=ind_shuffle)
+np.savez("../ind_shuffle_payne_h3.npz", ind_shuffle=ind_shuffle)
 
 #----------------------------------------------------------------------------------------
 # separate into training and validation set
@@ -30,4 +31,4 @@ validation_labels = labels[ind_shuffle,:][20000:,:]
 training_loss, validation_loss = training.neural_net(training_labels, training_spectra,\
                                                      validation_labels, validation_spectra,\
                                                      num_neurons=300, learning_rate=1e-4,\
-                                                     num_steps=1e5, batch_size=512, num_pixel=4433)
+                                                     num_steps=1e5, batch_size=512, num_pixel=4375)
