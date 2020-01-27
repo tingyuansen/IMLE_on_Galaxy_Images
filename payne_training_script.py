307a847fa934e5905ca2ffee667015bedcc9c937
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-27 14:48:28 -0500

    Update

--- payne_training_script.py
+++ payne_training_script.py
@@ -7,7 +7,8 @@ from The_Payne import training
 # read spectra
 temp = np.load("../H3_training_grid.npz")
 #spectra = temp["spectra"][:,150:4433+150]
-spectra = temp["spectra"][:,150:4375+150]
+#spectra = temp["spectra"][:,150:4375+150]
+spectra = temp["spectra"]
 labels = temp["labels"]
 labels[:,0] = labels[:,0]/1000.
 
@@ -21,14 +22,14 @@ np.savez("../ind_shuffle_payne_h3.npz", ind_shuffle=ind_shuffle)
 
 #----------------------------------------------------------------------------------------
 # separate into training and validation set
-training_spectra = spectra[ind_shuffle,:][:20000,:]
-training_labels = labels[ind_shuffle,:][:20000,:]
-validation_spectra = spectra[ind_shuffle,:][20000:,:]
-validation_labels = labels[ind_shuffle,:][20000:,:]
+training_spectra = spectra[ind_shuffle,:][:12000,:]
+training_labels = labels[ind_shuffle,:][:12000,:]
+validation_spectra = spectra[ind_shuffle,:][12000:,:]
+validation_labels = labels[ind_shuffle,:][12000:,:]
 
 #----------------------------------------------------------------------------------------
 # train neural network # require GPU
 training_loss, validation_loss = training.neural_net(training_labels, training_spectra,\
                                                      validation_labels, validation_spectra,\
                                                      num_neurons=300, learning_rate=1e-4,\
-                                                     num_steps=1e5, batch_size=512, num_pixel=4375)
+                                                     num_steps=1e6, batch_size=512, num_pixel=spectra.shape[1])
