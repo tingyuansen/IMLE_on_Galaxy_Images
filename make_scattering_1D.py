27e3a018f80df7c67cf6b2d4af5a87ae8bdb28cf
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-17 22:52:34 +0000

    Update

---
+++ make_scattering_1D.py
@@ -0,0 +1,30 @@
+# import packages
+from kymatio import Scattering1D
+import kymatio
+
+import torch.nn as nn
+import torch.optim
+import torch
+import torch.utils.data as utils
+
+import time
+import sys
+
+import numpy as np
+
+
+#=========================================================================================================
+# load light curves
+light_curve = np.load("../light_curve.npy")
+
+# define wavelet scattering hyperparameters
+J = 12
+Q = 5
+
+# convert into torch variable
+x = torch.from_numpy(light_curve).type(torch.FloatTensor)
+
+# perform wavelet scattering
+scattering = Scattering1D(J, light_curve.shape[1], Q)
+s = scattering.forward(x)[0]
+print(s.shape)
