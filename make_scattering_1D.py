b8785b5f07592e314a30fd5d111466c53ab2da25
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-18 14:39:42 +0000

    Update

--- make_scattering_1D.py
+++ make_scattering_1D.py
@@ -22,7 +22,7 @@ print(real_spec.shape)
 #real_spec = real_spec*10.
 
 ## mix two modes
-real_spec = (real_spec[:,:] + real_spec[::-1,:])
+#real_spec = (real_spec[:,:] + real_spec[::-1,:])
 
 
 #================================================================================================
@@ -56,4 +56,4 @@ Sx_all = np.log10(Sx_all[:,1:])
 print(Sx_all.shape)
 
 # save results
-np.save("../Sx_all_mixed.npy", Sx_all)
+np.save("../Sx_all_normal.npy", Sx_all)
