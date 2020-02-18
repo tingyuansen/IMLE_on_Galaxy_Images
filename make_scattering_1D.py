262fe7d1e16b38f88a5ea014db333b96c2e1d285
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-18 13:46:56 +0000

    Update

--- make_scattering_1D.py
+++ make_scattering_1D.py
@@ -13,7 +13,7 @@ import sys
 import numpy as np
 
 
-#=========================================================================================================
+#===============================================================================================
 # load light curves
 real_spec = np.load("../light_curve.npy")
 print(real_spec.shape)
@@ -23,6 +23,11 @@ real_spec = real_spec*2.
 
 ## mix two modes
 #real_spec = (real_spec[:,:] + real_spec[::-1,:])/2.
+
+#-----------------------------------------------------------------------------------------------
+# normalize wrt to the first coefficient
+for i in range(y_tr.shape[0]):
+    y_tr[i,:] = y_tr[i,:]/y_tr[i,0]
 
 
 #================================================================================================
@@ -43,11 +48,12 @@ scattering.cuda()
 # perform wavelet scattering
 Sx_all = scattering.forward(x)
 
-# calculate invariate representation
-Sx_all = torch.mean(Sx_all[:,1:,:], dim=-1)
+# normalize wrt to the first coefficient
+for i in range(Sx_all.shape[0]):
+    Sx_all[i,:] = Sx_all[i,:]/np.abs(Sx_all[i,0])
 
 # take log to normalize the coefficient better
-Sx_all = torch.log10(Sx_all)
+Sx_all = torch.log10(Sx_all[:,1:])
 print(Sx_all.shape)
 
 # save results
