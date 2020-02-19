01157bb4a75e7e9f55bc63c83dc5a61d800c90fb
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-19 15:51:01 +0000

    Update

--- Gaussian_Processes_Interpolaton.py
+++ Gaussian_Processes_Interpolaton.py
@@ -73,4 +73,4 @@ Y_predict_array = np.array(pool.map(GP_interp,range(100)))
 print(time.time()-start_time)
 
 # save results
-np.save("g_array_interp.npy", np.array(Y_predict_array))
+np.save("../g_array_interp.npy", np.array(Y_predict_array))
