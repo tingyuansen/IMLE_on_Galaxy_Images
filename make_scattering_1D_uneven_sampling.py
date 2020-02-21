7dd2b9c6be098d60d3d0d11d58b5f3330c061304
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-21 22:38:49 +0000

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -7,20 +7,12 @@ import numpy as np
 
 #===============================================================================================
 # load light curves
-# temp = np.load("../SDSS_DR14_qso_mock_normal_dense.npz")
-# real_spec = temp["light_curve"]
-# print(real_spec.shape)
-
-real_spec_all = np.load("../SDSS_DR14_qso_mock_mixed_dense_GP_interpolated.npy")
-print(real_spec_all.shape)
+temp = np.load("../SDSS_DR14_qso_mock_normal_dense.npz")
+real_spec = temp["light_curve"]
+print(real_spec.shape)
 
 ### change the amplitude
 #real_spec = real_spec*10.
-
-## mix two modes
-#real_spec = (real_spec[:,:] + real_spec[::-1,:])
-#real_spec = -2.5*np.log10(10**(-real_spec[:,:]/2.5)+ 10**(-real_spec[::-1,:]/2.5))
-
 
 #================================================================================================
 # choose windows of convolution (in unit of days)
@@ -33,36 +25,50 @@ Sx_all = []
 
 #-----------------------------------------------------------------------------------------------
 # loop over all objects
-for i in range(real_spec.shape[0]):
+#for j in range(real_spec.shape[0]):
+for j in range(100):
 
-    #
-# make smooth template
-real_spec_smooth = np.copy(real_spec)
+    print(j)
+    
+    # array collect coefficients
+    Sx_all_temp = []
 
-# loop over all pixels
-for i in range(real_spec.size):
+    # choose a spectrum
+    real_spec = real_spec_all[j,:]
 
-    choose = np.abs(time_stamp - time_stamp[i]) < window_array[0]
-    real_spec_smooth[i] = np.mean(real_spec[choose])
-    real_spec_2_smooth[i] = np.mean(real_spec_2[choose])
-    mixed_spec_smooth[i] = np.mean(mixed_spec[choose])
+    # normalize, i.e. substract away the zero order coefficients
+    real_spec = real_spec/np.mean(np.abs(real_spec))
 
-# substract away this frequency scale
-real_spec = real_spec - real_spec_smooth
-real_spec_2 = real_spec_2 - real_spec_2_smooth
-mixed_spec = mixed_spec - mixed_spec_smooth
+    # make smooth template
+    real_spec_smooth = np.copy(real_spec)
 
-# calculate invariate representation
-Sx_all = torch.mean(Sx_all, dim=-1)
-Sx_all = Sx_all.cpu().numpy()
+#-----------------------------------------------------------------------------------------------
+    # loop over all windows
+    for k in range(window_array.shape[0]):
 
-# normalize wrt to the first coefficient
-for i in range(Sx_all.shape[0]):
-    Sx_all[i,:] = Sx_all[i,:]/np.abs(Sx_all[i,0])
+        # record the pixels that are within the pixel
+        if k == 0:
+            choose_array = []
 
-# take log to normalize the coefficient better
-Sx_all = np.log10(Sx_all[:,1:])
-print(Sx_all.shape)
+#-----------------------------------------------------------------------------------------------
+        # loop over all pixels
+        for i in range(real_spec.size):
 
+            choose = np.abs(time_stamp - time_stamp[i]) < window_array[k]
+            real_spec_smooth[i] = np.mean(real_spec[choose])
+            choose_array.append(choose)
+
+#-----------------------------------------------------------------------------------------------
+        # substract away this frequency scale
+        real_spec = real_spec - real_spec_smooth
+
+        # extract coefficients
+        Sx_all_temp.append(np.mean(np.abs(real_spec)))
+
+    # record results
+    Sx_all_temp = np.array(Sx_all_temp)
+    Sx_all.append(Sx_all_temp)
+
+#-----------------------------------------------------------------------------------------------
 # save results
-np.save("../Sx_all_mixed_order=1_GP_interpolated.npy", Sx_all)
+np.save("../Sx_all_mixed_dense.npy", Sx_all)
