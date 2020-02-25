795dfd9796e20beb732cef7625f1d25a853b89c3
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-24 19:31:03 -0500

    Update

--- real_nvp_1D_light_curve.py
+++ real_nvp_1D_light_curve.py
@@ -14,7 +14,8 @@ from scipy import interpolate
 
 #========================================================================================================
 # read scattering coefficents
-y_tr = np.log10(np.load("../Sx_all_normal_wst.npy"))
+#y_tr = np.log10(np.load("../Sx_all_normal_wst.npy"))
+y_tr = np.load("../Sx_all_normal_wst.npy")
 
 # read GP coefficients
 #y_tr = np.load("../kernel_param_mock_normal_dt=0.001.npy")
