8f4f97198bf8afb7d164a21b3787e752390d30c1
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-24 20:44:28 -0500

    Update

--- make_scattering_1D.py
+++ make_scattering_1D.py
@@ -23,7 +23,7 @@ print(real_spec.shape)
 # print(real_spec.shape)
 
 ### change the amplitude
-#real_spec = real_spec*10.
+real_spec = real_spec*10.
 #real_spec = real_spec + 100.
 
 ## mix two modes
@@ -68,4 +68,4 @@ Sx_all = np.log10(Sx_all[:,1:])
 print(Sx_all.shape)
 
 # save results
-np.save("../Sx_all_normal_wst.npy", Sx_all)
+np.save("../Sx_all_normal_wst_x10.npy", Sx_all)
