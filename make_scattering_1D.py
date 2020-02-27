c5aa6a2703b92e3b8dd924f572b962830ebf15fa
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-27 16:27:43 -0500

    Update

--- make_scattering_1D.py
+++ make_scattering_1D.py
@@ -15,7 +15,7 @@ import numpy as np
 
 #===============================================================================================
 # load light curves
-temp = np.load("../SDSS_DR14_qso_mock_mixed_dense.npz")
+temp = np.load("../SDSS_DR14_qso_mock_normal_dense.npz")
 real_spec = temp["light_curve"]
 print(real_spec.shape)
 
@@ -24,7 +24,7 @@ print(real_spec.shape)
 
 ### change the amplitude
 #real_spec = real_spec*10.
-#real_spec = real_spec + 100.
+real_spec = real_spec + 100.
 
 
 #================================================================================================
@@ -60,4 +60,4 @@ for i in range(Sx_all.shape[0]):
     Sx_all[i,:] = Sx_all[i,:] - Sx_all[i,0]
 
 # save results
-np.save("../Sx_all_mixed_dense.npy", Sx_all[:,1:])
+np.save("../Sx_all_normal_dense_add100.npy", Sx_all[:,1:])
