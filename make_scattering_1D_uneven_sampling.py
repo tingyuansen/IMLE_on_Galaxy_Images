892bc50b8cf8553bd92ce96ed2f7c412d988c343
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-27 02:20:01 -0500

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -8,7 +8,7 @@ from multiprocessing import Pool
 
 #===============================================================================================
 # load light curves
-temp = np.load("../SDSS_DR14_qso_mock_normal_dense_x10.npz")
+temp = np.load("../SDSS_DR14_qso_mock_normal_dense.npz")
 t_array = temp["t_array"]
 real_spec_all = temp["light_curve"]
 print(real_spec_all.shape)
