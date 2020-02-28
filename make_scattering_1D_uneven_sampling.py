a5b7eda49c927654091e59947a87b313bf5b7797
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-27 23:21:17 -0500

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -93,4 +93,4 @@ Sx_all = np.array(pool.map(calc_coefficient,range(real_spec_all.shape[0])))
 print(Sx_all.shape)
 
 # save results
-np.save("../Sx_all_normal_dense_x10.npy", Sx_all)
+np.save("../Sx_all_normal_dense.npy", Sx_all)
