dcfc708a4f903f6572709794638109bec215fae8
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-02-27 22:00:04 -0500

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -20,7 +20,7 @@ real_spec_all = temp["light_curve"][:,ztf_time]
 print(real_spec_all.shape)
 
 ### change the amplitude
-#real_spec_all = real_spec_all*10.
+real_spec_all = real_spec_all*10.
 #real_spec_all = real_spec_all + 100.
 
 # zero out the mean since WST is not addition invariant
@@ -87,4 +87,4 @@ Sx_all = np.array(pool.map(calc_coefficient,range(real_spec_all.shape[0])))
 print(Sx_all.shape)
 
 # save results
-np.save("../Sx_all_normal_dense.npy", Sx_all)
+np.save("../Sx_all_normal_dense_x10.npy", Sx_all)
