5c24f90ae37cbf3b7e8082ec21438fc09a28bcb0
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-03-02 11:08:06 -0500

    Update

--- real_nvp_1D_light_curve.py
+++ real_nvp_1D_light_curve.py
@@ -16,7 +16,7 @@ from scipy import interpolate
 # read scattering coefficents
 y_tr = np.load("../Sx_all_normal_dense.npy")
 #y_tr[y_tr == 0] = np.median(y_tr)
-y_tr = np.log10(y_tr)
+y_tr = np.log10(y_tr[:,:4])
 
 # convert into torch
 y_tr = torch.from_numpy(y_tr).type(torch.cuda.FloatTensor)
