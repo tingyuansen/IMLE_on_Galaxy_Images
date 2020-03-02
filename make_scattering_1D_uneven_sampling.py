2daeaf75c23eacefe44bcca714a3e78dbca4cc2f
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-03-02 15:42:09 -0500

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -20,6 +20,7 @@ t_array = temp["t_array"]
 real_spec_all = temp["light_curve"]
 print(real_spec_all.shape)
 
+
 ### change the amplitude
 #real_spec_all = real_spec_all*10.
 #real_spec_all = real_spec_all + 100.
