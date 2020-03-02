5356df6750d60ae6d2550c73f4264c72124f219a
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-03-02 15:00:27 -0500

    Update

--- make_scattering_1D_uneven_sampling.py
+++ make_scattering_1D_uneven_sampling.py
@@ -47,10 +47,10 @@ def calc_coefficient(j):
     choose_step = np.unique((ztf_time[j]*10).astype("int"))
 
     # choose a spectrum
-    real_spec = real_spec_all[j]
-    time_stamp = t_array[j]
-    #real_spec = real_spec_all[j][choose_step]
-    #time_stamp = t_array[j][choose_step]
+    # real_spec = real_spec_all[j]
+    # time_stamp = t_array[j]
+    real_spec = real_spec_all[j][choose_step]
+    time_stamp = t_array[j][choose_step]
 
     # make multiplicative invariant
     # by default it is additive invariant
