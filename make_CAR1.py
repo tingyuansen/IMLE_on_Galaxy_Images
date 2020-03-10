07576a32de1e7de7ab18f6dc173d42d3804bc4bf
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-03-10 15:08:35 -0400

    Update

--- make_CAR1.py
+++ make_CAR1.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool
 
 #=============================================================================================================
 # duration in day
-duration = 1000
+duration = 100
 
 # define time array
 t_clean = np.linspace(0, duration-0.1, int(duration*10)) # cadence of 0.1 day
